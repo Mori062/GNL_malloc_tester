a430fba9db79c58b28448c1cb9172a24493e437f
Mori062 <01morishi@gmail.com>
2023-09-26 19:24:24 +0900

    [ADD] sed.py

---
+++ sed.py
@@ -0,0 +1,11 @@
+import re
+
+file_path = "get_next_line.c"
+
+with open(file_path, 'r') as file:
+    file_content = file.read()
+
+modified_content = re.sub(r'\(char \*\)malloc', 'fmalloc', file_content)
+
+with open(file_path, 'w') as file:
+    file.write(modified_content)
