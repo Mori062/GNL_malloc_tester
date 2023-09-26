db2538d0bf01b1a3c44d3e5af73ef143594a3bfd
Mori062 <01morishi@gmail.com>
2023-09-26 18:40:41 +0900

    [Make] tester

---
+++ test.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 18:36:10 by morishitash       #+#    #+#             */
+/*   Updated: 2023/09/26 18:39:20 by morishitash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libc.h"
+# include "get_next_line.h"
+int	g_cnt = 0;
+#define N 0
+
+char	*fmalloc(int i)
+{
+	char	*str;
+
+	if (g_cnt == N)
+		str = NULL;
+	else
+		str = malloc(i);
+	g_cnt++;
+	return (str);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 0;
+	fd1 = open("get_next_line.c", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd1);
+		printf("line:[%d]    [%s]", i++, line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	close(fd1);
+	system("leaks -q gnl");
+	return (0);
+}
---
+++ tester.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cp ../get_next_line.c .
+cp ../get_next_line_utils.c .
+
+sed -i 's/malloc/fmalloc/g' get_next_line.c
+sed -i 's/malloc/fmalloc/g' get_next_line_utils.c
+
+for i in $(seq 0 50)
+do
+    cc -Wall -Werror -Wextra get_next_line_bonus.c get_next_line_utils_bonus.c -D N=$i -o gnl
+    ./gnl > /dev/null 2>&1
+    if [ $? != 0 ]
+    then
+        echo "\x1b[31m malloc KO \x1b[0m"
+    else
+        echo "\x1b[32m malloc OK \x1b[0m"
+    fi
+    i=$((i+1))
+    sleep 0.2
+    rm ./gnl
+done
